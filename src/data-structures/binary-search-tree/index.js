f03b3893292b5ea96d477874149fec6bee1653cf
Say-re <91860677+Say-re@users.noreply.github.com>
2022-12-21 14:26:23 -0700

    Implement binary search tree

---
+++ src/data-structures/binary-search-tree/index.js
@@ -0,0 +1,78 @@
+// @flow
+interface NodeClass {
+  value: number,
+  left: null | NodeClass,
+  right: null | NodeClass,
+}
+
+interface BSTClass {
+  root: null | NodeClass,
+}
+
+export class Node {
+  value: number;
+  left: null | NodeClass;
+  right: null | NodeClass;
+
+  constructor (value: number) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+export class BinarySearchTree {
+  root: NodeClass | null;
+  constructor () {
+    this.root = null;
+  }
+
+  find (val: number): boolean | NodeClass {
+    if (this.root === null) return false;
+    const traverseTree = (value: number, comparisonNode: NodeClass): boolean | NodeClass => {
+      if (!value || !comparisonNode) return false;
+      if (value === comparisonNode.value) return comparisonNode;
+      if ((value < comparisonNode.value && !comparisonNode.left) || (value > comparisonNode.value && !comparisonNode.right)) {
+        return false;
+      }
+      if (value < comparisonNode.value && comparisonNode.left) {
+        return traverseTree(value, comparisonNode.left);
+      }
+      if (value > comparisonNode.value && comparisonNode.right) {
+        return traverseTree(value, comparisonNode.right);
+      }
+      return false;
+    };
+    return traverseTree(val, this.root);
+  }
+
+  insert (value: number): BSTClass | null {
+    const newNode: NodeClass = new Node(value);
+    if (!this.root) {
+      this.root = newNode;
+    } else {
+      const traverseTree = (node: NodeClass, comparisonNode: NodeClass): null => {
+        if (!node || !comparisonNode) return null;
+        if (node.value < comparisonNode.value && !comparisonNode.left) {
+          comparisonNode.left = node;
+          return null;
+        }
+        if (node.value > comparisonNode.value && !comparisonNode.right) {
+          comparisonNode.right = node;
+          return null;
+        }
+        if (node.value < comparisonNode.value && comparisonNode.left) {
+          traverseTree(node, comparisonNode.left);
+        }
+        if (node.value > comparisonNode.value && comparisonNode.right) {
+          traverseTree(node, comparisonNode.right);
+        }
+        return null;
+      };
+      traverseTree(newNode, this.root);
+    }
+    return this;
+  }
+}
+
+export default BinarySearchTree;
