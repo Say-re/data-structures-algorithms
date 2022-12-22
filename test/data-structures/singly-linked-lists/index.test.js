619fbb1ef0be8f561cf09918a6f9aeb1b5131f63
Say-re <91860677+Say-re@users.noreply.github.com>
2022-12-21 20:18:45 -0700

    Test: Complete testing for doubly linked lists

---
+++ test/data-structures/singly-linked-lists/index.test.js
@@ -0,0 +1,46 @@
+// import { SinglyLinkedList, Node } from '../../../src/data-structures/singly-linked-lists';
+
+// describe('src/data-structures/singly-linked-lists/index.js', () => {
+//   it('Verifies defualt attributes of Node instance', () => {
+//     const sampleNode = new Node(900);
+//     expect(sampleNode.val)
+//       .toEqual(900);
+//     expect(sampleNode.prev)
+//       .toEqual(null);
+//     expect(sampleNode.next)
+//       .toEqual(null);
+//   });
+//   it('Verifies default attributes of SinglyLinkedList instance', () => {
+//     const sampleSinglyLinkedList = new SinglyLinkedList();
+//     expect(sampleSinglyLinkedList.length)
+//       .toEqual(0);
+//     expect(sampleSinglyLinkedList.next)
+//       .toEqual(null);
+//     expect(sampleSinglyLinkedList.prev)
+//       .toEqual(null);
+//   });
+//   it('Verifies functionality of method - Get - for SinglyLinkedLists'), () => {
+
+//   };
+//   it('Verifies functionality of method - Insert - for SinglyLinkedLists'), () => {
+
+//   };
+//   it('Verifies functionality of method - Pop - for SinglyLinkedLists'), () => {
+
+//   };
+//   it('Verifies functionality of method - Push - for SinglyLinkedLists'), () => {
+
+//   };
+//   it('Verifies functionality of method - Remove - for SinglyLinkedLists'), () => {
+
+//   };
+//   it('Verifies functionality of method - Set - for SinglyLinkedLists'), () => {
+
+//   };
+//   it('Verifies functionality of method - Shift - for SinglyLinkedLists'), () => {
+
+//   };
+//   it('Verifies functionality of method - Unshift - for SinglyLinkedLists'), () => {
+
+//   };
+// });
